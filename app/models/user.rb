0389665deb87e56ed7bd1c329f9ee2dee37073f4
kaisan-diary <75722248+kaisan-diary@users.noreply.github.com>
2021-02-01 19:28:00 +0900

    プルダウンの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
+  
+  has_many :room_users
+  has_many :rooms, through: :room_users
 
 end
